6d4046a29c26e5c6bbc33c4a5aa9746d7182928b
Parvej Hosen <parvej@ticonsys.com>
2020-10-11 14:17:53 +0600

    initial commit

---
+++ interactive_check.py
@@ -0,0 +1,9 @@
+ from parlai.scripts.interactive import Interactive
+
+# call it with particular args
+Interactive.main(
+    # the model_file is a filename path pointing to a particular model dump.
+    # Model files that begin with "zoo:" are special files distributed by the ParlAI team.
+    # They'll be automatically downloaded when you ask to use them.
+    model_file='zoo:tutorial_transformer_generator/model'
+)
